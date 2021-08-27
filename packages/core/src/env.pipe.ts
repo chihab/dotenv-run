5aca5525a0fcd413e2eec1e230f121262554d67a
Abderrahim SOUBAI-ELIDRISI <gx.coder@gmail.com>
2021-08-28 00:55:08 +0100

    chore(core): pipe enhancement

--- packages/core/src/env.pipe.ts
+++ packages/core/src/env.pipe.ts
@@ -2,10 +2,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'env',
+  pure: true,
 })
 export class EnvPipe implements PipeTransform {
-  transform(variable: string): unknown {
-    const _variable = variable.replace('process.env.', '');
+  transform(variable: string): string {
+    const _variable = variable.replace(/^process.env./i, '');
     return _variable === 'NODE_ENV'
       ? process.env.NODE_ENV
       : process.env[_variable];
