69500409ff776408226748d5c6c62a29a788550a
Viktor Chernodub <37013688+chernodub@users.noreply.github.com>
2021-11-22 21:44:34 +0700

    fix: improve globally-declared env typing
    Resolves #8

--- packages/builder/src/schematics/ng-add/template/env.d.ts
+++ packages/builder/src/schematics/ng-add/template/env.d.ts
@@ -1,3 +1,14 @@
-declare let process: {
-  env: { [key: string]: any };
-};
+declare namespace NodeJS {
+
+  /** Merge declaration with `process` in order to override the global-scoped env. */
+  export interface ProcessEnv {
+
+    /**
+     * Built-in environment variable.
+     * @see Docs https://github.com/chihab/ngx-env#ng_app_env.
+     */
+    readonly NG_APP_ENV: string;
+
+    // Add your environment variables below 
+  }
+}
