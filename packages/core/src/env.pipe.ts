89612b0a40a4b8f78a71d110a5643481eaafb57a
c.otmani <chihab@gmail.com>
2022-11-28 08:51:01 +0100

    chore: update to ng 15

--- packages/core/src/env.pipe.ts
+++ packages/core/src/env.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "env",
 })
 export class EnvPipe implements PipeTransform {
-  transform(variable: string): string {
+  transform(variable: string): string | undefined {
     const _variable = variable.replace(/^process.env./, "");
     return _variable === "NG_APP_ENV"
       ? process.env.NG_APP_ENV
