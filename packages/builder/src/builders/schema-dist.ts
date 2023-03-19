5e525303ebea1cc453e6d6d90827e8646e00da70
c.otmani <chihab@gmail.com>
2023-03-19 08:46:22 +0000

    chore: upgrade demo-app to 15.2.3

--- packages/builder/src/builders/schema-dist.ts
+++ packages/builder/src/builders/schema-dist.ts
@@ -5,13 +5,13 @@ function copyDistSchemas() {
   console.log("Copying Angular CLI target schemas");
   ["dev-server", "browser", "extract-i18n", "karma", "server"].forEach(
     async (target) => {
-      console.log(`${target}/schema.json ✅`);
       await cpy(
         [
           `../../node_modules/@angular-devkit/build-angular/src/builders/${target}/schema.json`,
         ],
         `src/builders/${target}`
       );
+      console.log(`../../node_modules/@angular-devkit/build-angular/src/builders/${target}/schema.json ==> src/builders/${target} ✅`);
       fs.renameSync(
         `src/builders/${target}/schema.json`,
         `src/builders/${target}/${target}.json`
