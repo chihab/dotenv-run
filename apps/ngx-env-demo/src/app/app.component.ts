6f16a971f62a7e9435414bd0342319a1551f7763
c.otmani <chihab@gmail.com>
2023-03-25 17:55:12 +0000

    chore: reorganize workspaces

--- apps/ngx-env-demo/src/app/app.component.ts
+++ apps/ngx-env-demo/src/app/app.component.ts
@@ -7,13 +7,10 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  env = process.env.NG_APP_ENV;
+  env = process.env['NG_APP_ENV'];
   version = environment.env.NGX_VERSION;
-  branch = process.env.NGX_BRANCH_NAME;
-  appHome = process.env.NGX_USER_HOME;
-  home = process.env.USER_HOME;
-  notInTest = process.env.NGX_NOT_IN_TEST;
-  constructor() {
-    console.log(process.env);
-  }
+  branch = process.env['NGX_BRANCH_NAME'];
+  appHome = process.env['NGX_USER_HOME'];
+  home = process.env['USER_HOME'];
+  notInTest = process.env['NGX_NOT_IN_TEST'];
 }
