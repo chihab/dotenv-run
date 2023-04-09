60008e99b06b1fd7a5601762371bed6ba7b1fea2
c.otmani <chihab@gmail.com>
2023-04-09 12:10:54 +0000

    fix(schematics): add both declarations to handle @types/node presence

--- packages/builder/src/schematics/ng-add/template/env.d.ts
+++ packages/builder/src/schematics/ng-add/template/env.d.ts
@@ -1,12 +1,26 @@
-declare namespace NodeJS {
-  /** Merge declaration with `process` in order to override the global-scoped env. */
-  export interface ProcessEnv {
-    /**
-     * Built-in environment variable.
-     * @see Docs https://github.com/chihab/ngx-env#ng_app_env.
-     */
-    readonly NG_APP_ENV: string;
+// This should work in most cases
+declare var process: {
+  env: {
+    NG_APP_ENV: string;
+    [key: string]: any;
+  };
+};
 
+// If your project references @types/node directly (in you) or indirectly (as in RxJS < 7.6.0), 
+// you might need to use the following declaration merging.
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    readonly NG_APP_ENV: string;
     // Add your environment variables below
   }
 }
+
+// If you're using Angular Universal, you'll need to add the following to your tsconfig.server.json:
+/* In your tsconfig.server.json */ 
+// {
+//   "extends": "./tsconfig.app.json",
+//   ...
+//   "exclude": [
+//     "src/env.d.ts"
+//   ]
+// }
