67f662b4826098645fc7b697c63cdf84d527fbd5
Luca Peruzzo <luca.peruzzo@sharry.land>
2023-11-13 08:24:53 +0100

    add demo repo for angular v17

---
+++ apps/ngx-env-v17-demo/src/app/app.config.ts
@@ -0,0 +1,9 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideClientHydration()]
+};
---
+++ apps/ngx-env-v17-demo/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  env: {
+    NGX_VERSION: import.meta.env['NGX_VERSION'],
+  },
+};
