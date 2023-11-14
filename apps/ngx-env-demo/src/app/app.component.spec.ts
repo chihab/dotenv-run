963334dfe531cdfb981e2e0d4409ffd7c9c30e66
Luca Peruzzo <luca.peruzzo@sharry.land>
2023-11-14 11:40:05 +0100

    no title in code

--- apps/ngx-env-demo/src/app/app.component.spec.ts
+++ apps/ngx-env-demo/src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ngx-env-demo');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ngx-env-demo');
-  });
 });
