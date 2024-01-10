26fca1824091aa2918068573b3a516f5f87eb6c5
Chihab Otmani <chihab@gmail.com>
2024-01-10 15:26:32 +0100

    feat: add secure mode in debug (#68)
    * feat(core): add secure mode in debug

--- packages/core/src/options.ts
+++ packages/core/src/options.ts
@@ -1,12 +1,15 @@
 import type { DotenvConfigOptions } from "dotenv";
+import { Dict } from "./build";
 
 export interface DotenvRunOptions {
   cwd?: string; // Path to current working directory
   dotenv?: DotenvConfigOptions;
   environment?: string; // Environment to load
-  files?: string | string[]; // Environment files to load
+  files?: string[]; // Environment files to load
   prefix?: string | RegExp; // Filter keys to inject
+  unsecure?: boolean; // Display environment variables in debug output
   root?: string; // Path to root workspace
   nodeEnv?: boolean; // Node environment
   verbose?: boolean; // Print verbose output
+  builtIn?: Dict; // Built-in environment variables
 }
