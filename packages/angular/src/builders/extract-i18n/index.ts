13bb14d3fe9c7d4dded889984ca5ad9788654028
Chihab Otmani <chihab@gmail.com>
2024-02-28 18:43:17 +0100

    fix(angular): fix ignored template errors

--- packages/angular/src/builders/extract-i18n/index.ts
+++ packages/angular/src/builders/extract-i18n/index.ts
@@ -9,6 +9,7 @@ import {
 } from "@angular-devkit/build-angular";
 import { combineLatest, switchMap } from "rxjs";
 import { NgxEnvSchema } from "../ngx-env/ngx-env-schema";
+import { JsonObject } from "@angular-devkit/core";
 
 export const buildWithPlugin = (
   options: ExtractI18nBuilderOptions & NgxEnvSchema,
@@ -21,7 +22,7 @@ export const buildWithPlugin = (
   async function setup() {
     const targetOptions = await context.getTargetOptions(buildTarget);
     if ((await builderName()) === "@ngx-env/builder:application") {
-      // Because of validateOptions being ignored, we need to validate options manually
+      // Because of ngxEnv being removed from the options, we need to validate it here
       await context.validateOptions(
         targetOptions,
         "@ngx-env/builder:application"
@@ -37,11 +38,12 @@ export const buildWithPlugin = (
       if (builderName === "@ngx-env/builder:browser") {
         return executeExtractI18nBuilder(options, context);
       } else {
+        delete _options.ngxEnv;
+        context.getTargetOptions = async () => _options as JsonObject;
         return executeExtractI18nBuilder(options, {
           ...context,
           getBuilderNameForTarget: async () =>
             "@angular-devkit/build-angular:" + builderName.split(":")[1],
-          validateOptions: async (options) => options as any,
         });
       }
     })
